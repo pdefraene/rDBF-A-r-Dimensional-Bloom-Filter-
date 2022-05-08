b53a6ee26ef110ee654f33600d38ba1a415a379f
pdefraene <pierredefraene@gmail.com>
2022-05-08 15:35:50 +0200

    base 2DBF

---
+++ RDimensionalBloomFilter.py
@@ -0,0 +1,35 @@
+
+class TwoDBF:
+    def __init__(self,X,Y):
+        self.createBloomFilter(X,Y)
+
+    def createBloomFilter(self,X,Y):
+        pass
+
+    def set(self,i,j,pos):
+        pass
+
+    def test(self,i,j,pos):
+        pass
+
+    def setCount(self):
+        pass
+
+    def isFull(self):
+        pass
+
+    def setMember(self,word):
+        pass
+
+    def testMember(self,word):
+        pass
+
+    def delete(self,i,j,pos):
+        pass
+
+    def deleteMember(self,word):
+        pass
+
+
+if __name__ == "__main__":
+    TwoDBF(5,7)
